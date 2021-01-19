4885540aba30b7cdea18ca717a461241b17c449f
estherxin13 <56760895+estherxin13@users.noreply.github.com>
2021-01-18 20:59:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Dish } from "./Dish";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Esther's Restaurant</h1>
+      <Dish name = "Pizza 🍕"/>
+      <Dish name = "Fries 🍟"/>
+      <Dish name = "soup 🍜"/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Dish.js
@@ -0,0 +1,30 @@
+import React from "react"; 
+
+export class Dish extends React.Component {
+
+    constructor(props){
+        super(props); 
+        this.state = {
+            counter: 0
+        }; 
+
+    }; 
+
+    addDish = () => {
+        this.setState({
+            counter: this.state.counter+1
+        })
+    }
+
+    render (){
+        return (
+            <div className = "Dish">
+                <h2>{this.props.name}</h2>
+                <p>Amount: {this.state.counter}</p>
+                <button onClick = {this.addDish}>Add Dish</button>
+                </div>
+        );
+    }
+
+
+}
